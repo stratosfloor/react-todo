828e2d4979bc0ca0c82cda4eb79b93ce33257af4
Emil Noren <stratosfloor@hotmail.com>
2022-03-16 10:21:03 +0100

    Use input text for new task

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const tasks = [
+  const [text, setText] = useState("");
+  const [tasks, setTasks] = useState([
     { label: "Learn HTML" },
     { label: "Learn CSS" },
     { label: "Learn JS" },
-  ];
+  ]);
 
   const taskElements = tasks.map((task) => {
     return (
       <li key={task.label}>
         <span className="label">{task.label}</span>
+        <span className="trashcan">🗑️</span>
       </li>
     );
   });
+
+  const onClickOk = () => {
+    setTasks([
+      ...tasks,
+      {
+        label: text,
+      },
+    ]);
+  };
 
   return (
     <div className="App">
       <div class="container">
         <h1>My ToDo</h1>
         <p class="counter">0 completed</p>
-        <input type="text" />
-        <button>OK</button>
+        <input
+          type="text"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+        />
+        <button onClick={onClickOk}>OK</button>
         <small class="error"></small>
         <ul>{taskElements}</ul>
       </div>
