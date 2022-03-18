9306e42ffc9b1fdef4894e54fd17c9f5ba1beedd
Emil Noren <stratosfloor@hotmail.com>
2022-03-18 14:16:39 +0100

    Implemented count of tasks completed

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,11 +1,17 @@
 import TaskItem from "./TaskItem";
 
 function TaskList({ tasks }) {
-  const taskElements = tasks.map(task => {
-    return <TaskItem key={ task.label } label={ task. label } />;
-  })
+  const taskElements = tasks.map((task) => {
+    return (
+      <TaskItem
+        key={task.label}
+        label={task.label}
+        completed={task.completed}
+      />
+    );
+  });
 
-  return (<ul>{taskElements}</ul>);
+  return <ul>{taskElements}</ul>;
 }
 
 export default TaskList;
